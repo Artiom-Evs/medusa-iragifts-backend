51da83fb7f43bfd6b20817869de8284e18749f14
Artiom-Evs <artiom.evs@gmail.com>
2025-01-27 21:19:13 +0100

    Configure redis cache

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,16 +1,42 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig, ModuleRegistrationName } from '@medusajs/framework/utils';
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 
 module.exports = defineConfig({
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+    projectConfig: {
+        databaseUrl: process.env.DATABASE_URL,
+        redisUrl: process.env.REDIS_URL,
+        http: {
+            storeCors: process.env.STORE_CORS!,
+            adminCors: process.env.ADMIN_CORS!,
+            authCors: process.env.AUTH_CORS!,
+            jwtSecret: process.env.JWT_SECRET || 'supersecret',
+            cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+        },
+    },
+    modules: [
+        {
+            resolve: '@medusajs/cache-redis',
+            key: ModuleRegistrationName.CACHE,
+            options: {
+                redisUrl: process.env.REDIS_URL,
+            },
+        },
+        {
+            resolve: '@medusajs/event-bus-redis',
+            key: ModuleRegistrationName.EVENT_BUS,
+            options: {
+                redisUrl: process.env.REDIS_URL,
+            },
+        },
+        {
+            resolve: '@medusajs/workflow-engine-redis',
+            key: ModuleRegistrationName.WORKFLOW_ENGINE,
+            options: {
+                redis: {
+                    url: process.env.REDIS_URL,
+                },
+            },
+        },
+    ],
+});
