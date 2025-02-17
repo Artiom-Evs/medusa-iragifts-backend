dafc760152031cc390ee662e9498fd2b570c06aa
Artiom-Evs <artiom.evs@gmail.com>
2025-02-17 13:29:22 +0100

    Fix problem with "seed" script
    This script wasn't updated while downgrading Medusa.JS version to 2.4

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -205,7 +205,7 @@ export default async function seedDemoData({ container }: ExecArgs) {
                 rules: [
                     {
                         attribute: "enabled_in_store",
-                        value: "true",
+                        value: '"true"',
                         operator: "eq",
                     },
                     {
@@ -243,7 +243,7 @@ export default async function seedDemoData({ container }: ExecArgs) {
                 rules: [
                     {
                         attribute: "enabled_in_store",
-                        value: "true",
+                        value: '"true"',
                         operator: "eq",
                     },
                     {
@@ -323,7 +323,6 @@ export default async function seedDemoData({ container }: ExecArgs) {
                     handle: "t-shirt",
                     weight: 400,
                     status: ProductStatus.PUBLISHED,
-                    shipping_profile_id: shippingProfile.id,
                     images: [
                         {
                             url: "https://medusa-public-images.s3.eu-west-1.amazonaws.com/tee-black-front.png",
@@ -508,7 +507,6 @@ export default async function seedDemoData({ container }: ExecArgs) {
                     handle: "sweatshirt",
                     weight: 400,
                     status: ProductStatus.PUBLISHED,
-                    shipping_profile_id: shippingProfile.id,
                     images: [
                         {
                             url: "https://medusa-public-images.s3.eu-west-1.amazonaws.com/sweatshirt-vintage-front.png",
@@ -607,7 +605,6 @@ export default async function seedDemoData({ container }: ExecArgs) {
                     handle: "sweatpants",
                     weight: 400,
                     status: ProductStatus.PUBLISHED,
-                    shipping_profile_id: shippingProfile.id,
                     images: [
                         {
                             url: "https://medusa-public-images.s3.eu-west-1.amazonaws.com/sweatpants-gray-front.png",
@@ -706,7 +703,6 @@ export default async function seedDemoData({ container }: ExecArgs) {
                     handle: "shorts",
                     weight: 400,
                     status: ProductStatus.PUBLISHED,
-                    shipping_profile_id: shippingProfile.id,
                     images: [
                         {
                             url: "https://medusa-public-images.s3.eu-west-1.amazonaws.com/shorts-vintage-front.png",
